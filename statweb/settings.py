12d4209eb644954198daaaf6ea83818a14363f7a
Raushan Kumar <40159522+raushannanda@users.noreply.github.com>
2022-04-04 05:35:44 +0530

    Update settings.py

--- statweb/settings.py
+++ statweb/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-CSRF_TRUSTED_ORIGINS = ['https://statquery.herokuapp.com/'.https://www.statquery.herokuapp.com/]
+CSRF_TRUSTED_ORIGINS = ['https://statquery.herokuapp.com/','https://www.statquery.herokuapp.com']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
