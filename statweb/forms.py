2e875fc91e1c9420a8ad34bd694357520492dbeb
Raushan Kumar <40159522+raushannanda@users.noreply.github.com>
2022-04-16 23:33:48 +0530

    Add files via upload

--- statweb/forms.py
+++ statweb/forms.py
@@ -3,8 +3,8 @@ from email import message
 from logging import PlaceHolder
 from django import forms
 class SubscribeForm(forms.Form):
-    email = forms.EmailField()
+    email = forms.EmailField(label="email",widget=forms.EmailInput(attrs={'placeholder': 'Email Address'}))
 
 class ContactForm(forms.Form):
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField(label="email",widget=forms.EmailInput(attrs={'placeholder': 'Email Address'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message'}))
