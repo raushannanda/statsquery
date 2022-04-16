23047b5d246eee807bdfac006f97c0635907b717
Raushan Kumar <40159522+raushannanda@users.noreply.github.com>
2022-04-16 20:15:13 +0530

    Add files via upload

--- statweb/forms.py
+++ statweb/forms.py
@@ -1,5 +1,10 @@
 import email
+from email import message
 from logging import PlaceHolder
 from django import forms
 class SubscribeForm(forms.Form):
     email = forms.EmailField()
+
+class ContactForm(forms.Form):
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
--- statweb/views.py
+++ statweb/views.py
@@ -1,9 +1,12 @@
 
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect 
 from vlog.models import Post
 
 from .forms import SubscribeForm
+from .forms import ContactForm
 from vlog.models import SubEmail
+from vlog.models import Contact
+
 
 
 def home(request):
@@ -27,7 +30,18 @@ def terms(request):
     return render(request,'home/terms.html')
 
 def contact(request):
-    return render(request,'home/contact.html')
+    fm = ContactForm()
+    ctx = {'form':fm}
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            e = Contact()
+            e.email = form.cleaned_data['email']
+            e.message = form.cleaned_data['message']
+            e.save()
+            return render(request,'home/thanks.html')
+    else:    
+        return render(request,'home/contact.html',ctx)
 
 def about(request):
-    return render(request,'home/about.html')
+   return render(request,'home/about.html')
